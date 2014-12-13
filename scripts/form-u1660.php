c97621d4478db8041b337e59e158118c4198265a
transshipping <josemg3230@gmail.com>
2014-12-13 11:26:19 -0600

    actualización 131214

--- scripts/form-u1660.php
+++ scripts/form-u1660.php
@@ -21,16 +21,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 function emailFormSubmission()
 {
 	$to = 'info@tsg.com.gt';
-	$subject = 'Envío de Formulario Contacto';
+	$subject = 'Envío de Formulario Contacto Servicio al cliente';
 	
 	$message = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/><title>' . htmlentities($subject,ENT_COMPAT,'UTF-8') . '</title></head>';
 	$message .= '<body style="background-color: #ffffff; color: #000000; font-style: normal; font-variant: normal; font-weight: normal; font-size: 12px; line-height: 18px; font-family: helvetica, arial, verdana, sans-serif;">';
 	$message .= '<h2 style="background-color: #eeeeee;">Envío de nuevo formulario</h2><table cellspacing="0" cellpadding="0" width="100%" style="background-color: #ffffff;">'; 
-	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Nombre:</b></td><td>' . htmlentities($_REQUEST["custom_U447"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Nombre:</b></td><td>' . htmlentities($_REQUEST["custom_U1661"],ENT_COMPAT,'UTF-8') . '</td></tr>';
 	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Correo electrónico:</b></td><td>' . htmlentities($_REQUEST["Email"],ENT_COMPAT,'UTF-8') . '</td></tr>';
-	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Mensaje:</b></td><td>' . htmlentities($_REQUEST["custom_U455"],ENT_COMPAT,'UTF-8') . '</td></tr>';
-	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Asunto:</b></td><td>' . htmlentities($_REQUEST["custom_U474"],ENT_COMPAT,'UTF-8') . '</td></tr>';
-	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Teléfono móvil:</b></td><td>' . htmlentities($_REQUEST["custom_U481"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Mensaje:</b></td><td>' . htmlentities($_REQUEST["custom_U1669"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Asunto:</b></td><td>' . htmlentities($_REQUEST["custom_U1679"],ENT_COMPAT,'UTF-8') . '</td></tr>';
+	$message .= '<tr><td valign="top" style="background-color: #ffffff;"><b>Teléfono móvil:</b></td><td>' . htmlentities($_REQUEST["custom_U1674"],ENT_COMPAT,'UTF-8') . '</td></tr>';
 
 	$message .= '</table><br/><br/>';
 	$message .= '<div style="background-color: #eeeeee; font-size: 10px; line-height: 11px;">Formulario enviado desde el sitio web: ' . htmlentities($_SERVER["SERVER_NAME"],ENT_COMPAT,'UTF-8') . '</div>';
